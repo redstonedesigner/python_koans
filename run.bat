68c014b3136acd9771d7db09c350738ec936f29f
redstonedesigner <redstonedesigner1@gmail.com>
2021-05-08 23:53:50 +0100

    fix: Adjust run.bat to work on my system

--- run.bat
+++ run.bat
@@ -5,34 +5,14 @@ REM You don't actually need this script!
 SET RUN_KOANS=python.exe -B contemplate_koans.py
 
 REM Set this to your python folder:
-SET PYTHON_PATH=C:\Python39
+SET PYTHON_PATH="D:\Program Files\Python39"
 
-set SCRIPT=
+set SCRIPT=%PYTHON_PATH%\%RUN_KOANS%
 
 :loop
 
-REM Hunt around for python
-IF EXIST "python.exe" (
-  SET SCRIPT=%RUN_KOANS%
-) ELSE (
-  IF EXIST "%PYTHON_PATH%" (
-    SET SCRIPT=%PYTHON_PATH%\%RUN_KOANS%
-  ) ELSE (
-    IF EXIST %PYTHON% SET SCRIPT=%PYTHON%\%RUN_KOANS%
-  )
-)
-
 IF NOT "" == "%SCRIPT%" (
   %SCRIPT%
-  pause
-) ELSE (
-  echo.
-  echo Python.exe is not in the path!
-  echo.
-  echo Fix the path and try again.
-  echo Or better yet, run this with the correct python path:
-  echo.
-  echo   python.exe contemplate_koans.py
   pause
 )
 
